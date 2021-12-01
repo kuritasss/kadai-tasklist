9e2334665e01ba58309840df02d15bddf0fd317d
kurita <cider.sss723@gmail.com>
2021-12-01 17:50:50 +0000

    top page

---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,26 @@
+<header class="mb-4">
+            <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+                {{-- トップページへのリンク --}}
+                <a class="navbar-brand" href="/">TaskList</a>
+                
+                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                
+                <div class="collapse navbar-collapse" id="nav-bar">
+                    <ul class="navbar-nav mr-auto"></ul>
+                    <ul class="navbar-nav">
+                        @if(Auth::check())
+                            {{-- タスク作成ページへのリンク --}}
+                            <li class="nav-item">{!! link_to_route('tasks.create', '新規タスクの投稿', [], ['class' => 'nav-link']) !!}</li>
+                        @else
+                            {{-- ユーザー登録ページへのリンク --}}
+                            <li class="nav-item">{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
+                            
+                            {{-- ログインページへのリンク --}}
+                            <li class="nav-item">{!! link_to_route('signup.post', 'Login', [], ['class' => 'nav-link']) !!}</li>
+                        @endif
+                    </ul>
+                </div>
+            </nav>
+        </header>
